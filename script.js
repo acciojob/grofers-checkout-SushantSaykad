fdce4808d59a1bb7b90cde18d7a4948e707f2b96
Sushant Saykad <69181604+SushantSaykad@users.noreply.github.com>
2026-05-15 14:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
+
 document.body.appendChild(getSumBtn);
 
+const ans = document.createElement("h3");
+ans.id = "ans";
+document.body.appendChild(ans);
+
 const getSum = () => {
-//Add your code here
-  
+    const prices = document.querySelectorAll(".price");
+
+    let sum = 0;
+
+    prices.forEach(price => {
+        sum += parseFloat(price.textContent);
+    });
+
+    ans.textContent = sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
